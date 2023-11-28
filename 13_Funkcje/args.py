f5cafad73951f91e05bfeed6e1463187cae42450
localadmin <kazimierczak.anna14@gmail.com>
2023-11-28 15:03:18 +0100

    Zmiany po kursie dzien 2

--- 13_Funkcje/args.py
+++ 13_Funkcje/args.py
@@ -18,3 +18,21 @@ def test_var_kwargs(farg, **kwargs):
 
 
 test_var_kwargs(farg=1, myarg2="two", myarg3=3)
+
+#Zadanie1
+# stworzyc słownik `{ 'first': funkcja1, 'second': funkcja2 }`, wczytać przez `input` klucz, wywołać funkcję
+# zrób dwie funkcje i potem słownik i po wskazaniu klucza czy funkcja 1 czy 2 podaj wynik
+def funkcja1(x):
+    suma = 1 +1
+    return suma
+
+def funkcja2(x):
+    roznica = 4 - 3
+    return roznica
+
+slownik = { 'first': funkcja1, 'second': funkcja2 }
+
+klucz = input("Podaj klucz")
+
+funkcja = slownik[klucz]
+print(funkcja(2))
