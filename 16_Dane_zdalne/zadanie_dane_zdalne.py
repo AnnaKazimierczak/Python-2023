fd44e944ec53c45e64d9b3362b6b23a6bec096bd
localadmin <kazimierczak.anna14@gmail.com>
2023-11-29 13:50:39 +0100

    Zmiany po kursie dzien 3

--- 16_Dane_zdalne/zadanie_dane_zdalne.py
+++ 16_Dane_zdalne/zadanie_dane_zdalne.py
@@ -9,6 +9,36 @@ print()
 
 # print(25*"*")
 # print()
+powierzchnia= response.json()[0]['area']
+ludność= response.json()[0]['population']
+print(f'Powierzchnia: {powierzchnia} km2')
 print(response.json()[0]['area'])
 print(response.json()[0]['population'])
 print(response.json()[0]['currencies'])
+
+
+#Zadanie 2
+
+import requests
+from pprint import pprint
+import sys
+
+if __name__ == '__main__':
+    country = sys.argv[1]
+    print(sys.argv)
+    url = f"https://restcountries.com/v3.1/name/{country}?fullText=true"
+    response = requests.request(method="GET", url=url)
+
+    content = response.json()
+    content = content[0]
+    capital = content['capital'][0]
+    official1 = content['name']
+    official = official1['official']
+    print(official1)
+    population = content['population']
+    area = content['area']
+    print(f'Nazwa  : \t {official}')
+    print(f'Stolica: \t {capital}')
+    print(f'Ludnosc: \t {population}')
+    print(f'Pow.   : \t {area}')
+
