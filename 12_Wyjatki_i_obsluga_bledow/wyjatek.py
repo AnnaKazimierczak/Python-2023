815f276bd4aa3d7804d85b4287b6e75d0e2ecc1b
localadmin <kazimierczak.anna14@gmail.com>
2023-11-29 10:02:16 +0100

    Zmiany po kursie dzien 2

--- 12_Wyjatki_i_obsluga_bledow/wyjatek.py
+++ 12_Wyjatki_i_obsluga_bledow/wyjatek.py
@@ -9,3 +9,38 @@ else:
     print("Koniec")
 finally:
     print("Finally")
+
+#Zadanie- moje rozwiązanie- błąd
+
+n=0
+while True:
+    try:
+        n=int(input('podaj liczbe'))
+    except ValueError as e:
+        print('Zły format danych.Wpisz ponownie.')
+        print(e)
+    else:
+        braek
+
+n = int(input('podaj liczbe'))
+suma = n % 10 + n // 10
+print(f'suma liczy {n} wynosi {suma}')
+
+#Zadanie- inne rozwiązanie
+
+liczba = 0
+while True:
+    try:
+        liczba = input("Podaj liczbę naturalną: ")
+        liczba = int(liczba)
+    except ValueError as e:
+        print (f"{liczba} nie jest liczbą!")
+    else:
+        break
+
+suma = 0
+i = liczba
+while i > 0:
+    suma += i%10
+    i //= 10
+    print (f'Suma cyfr podanej liczby {liczba} wynosi {suma}')
